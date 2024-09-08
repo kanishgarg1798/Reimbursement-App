eb8e6e00a4d4ed9500bebd6a13b644fbda824b08
kanish garg <kanishgarg17@gmail.com>
2024-09-08 18:31:44 +0530

    changes in deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,7 @@ set :db_remote_clean, true
 set :ssh_options, {
   forward_agent: true,
   auth_methods: ["publickey"],
-  keys: ["~/.ssh/rid_rsa"]
+  keys: ["~/Downloads/reimburse-aws-test.pem"]
 
 }
 
